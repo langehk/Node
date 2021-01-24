8f6ce879b6accad027325c2d00c6b8cec4fc6c6f
langehk <32093132+langehk@users.noreply.github.com>
2021-01-24 15:46:35 +0100

    initial commit

---
+++ Node I.0a/funcModule.js
@@ -0,0 +1,14 @@
+
+ export function checkValues(param){
+
+    param.forEach(e => {
+        if(e != null && e != ""){
+            debugger;
+            return true;
+        }
+
+        else {
+        return false;
+        }
+    });
+}
---
+++ Node I.0a/value.js
@@ -0,0 +1,3 @@
+import {checkValues} from "./funcModule.js"
+
+checkValues(["dav", "dav2", 22, null]);
